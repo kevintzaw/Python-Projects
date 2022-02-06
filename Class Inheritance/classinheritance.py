17a033a21e303a6298699cbe76d017eed10c150d
kevintzaw <88251025+kevintzaw@users.noreply.github.com>
2022-02-05 22:52:31 -0500

    Create classinheritance.py

---
+++ Class Inheritance/classinheritance.py
@@ -0,0 +1,9 @@
+class westcoastEmployees:
+    #Define the attributes of the class
+    region = 'westcoast'
+    company = 'westcompany'
+
+class Manager(westcoastEmployees):
+    base_pay = 11.00
+    department = 'General'
+    
