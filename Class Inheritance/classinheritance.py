fec525cf54500a3cd5fb6c9a860287955992fddd
kevintzaw <88251025+kevintzaw@users.noreply.github.com>
2022-02-05 22:53:30 -0500

    Update classinheritance.py

--- Class Inheritance/classinheritance.py
+++ Class Inheritance/classinheritance.py
@@ -1,8 +1,10 @@
+#Define parent class
 class westcoastEmployees:
     #Define the attributes of the class
     region = 'westcoast'
     company = 'westcompany'
 
+#Define child class
 class Manager(westcoastEmployees):
     base_pay = 11.00
     department = 'General'
