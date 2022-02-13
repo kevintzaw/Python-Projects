63695d010c736745f24c57bdf92a1fdb01ac52aa
kevintzaw <88251025+kevintzaw@users.noreply.github.com>
2022-02-13 10:23:06 -0500

    main

--- Encapsulation/encapsulation.py
+++ Encapsulation/encapsulation.py
@@ -5,12 +5,18 @@ class protected:
     _name = "Kevin"
     _number = 23
 
+    #private variables
+    __privateVar = 27;
+
     #public member function
     def displayNameandNumber(self):
 
         #accessing protected data members
         print("Name: ", self._name)
         print("Roll: ", self._number)
+
+        #accessing private variable
+        print("Amount is ", protected.__privateVar)
 
 #creating object within a class
 obj = protected()
