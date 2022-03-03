f3863ed97532079d0d28751c6d64a2fc365ab075
kevintzaw <88251025+kevintzaw@users.noreply.github.com>
2022-03-03 18:26:46 -0500

    main

---
+++ Checkbox_Assignment/Django_Checkbook/Checkbook/urls.py
@@ -0,0 +1,10 @@
+from unicodedata import name
+from django.urls import path
+from . import views
+
+urlpatters = [
+    path('', views.home, name='index'),
+    path('creative/', views.create_account, name='create'),
+    path('balance/', views.balance, name='balance'),
+    path('transaction/', views.transaction, name='transaction')
+]
